df3a63bde4e0dfe04db8ac8868262c8f31ea2615
Huu Thang NGUYEN <huu-thang.nguyen@socgen.com>
2020-10-21 19:00:28 +0200

    feat: step 2

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 import "dotenv/config";
-const bodyParser = require('body-parser');
-const fetch = require("node-fetch");
-
 
 import makeCoordinateProvider from "./coordinate/coordinate.provider";
 import makeCoordinateService from "./coordinate/coordinate.service";
 import makeCoordinateController from "./coordinate/coordinate.controller";
 
-
 const coordinateProvider = makeCoordinateProvider({ fetch });
 const coordinateService = makeCoordinateService({ coordinateProvider });
 const coordinateController = makeCoordinateController({ coordinateService });
 
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/coordinate",coordinateController.get);
+app.get("/coordinate", coordinateController.get);
+
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDocument));
+
 module.exports = app;
